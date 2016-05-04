c21caf6ba114ab65e6cd439d2d6df1aeb6f3c7f6
qingwei.li <cinwell.li@gmail.com>
2016-05-04 16:09:36 +0800

    fix loader

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function (cooking, options) {
 
   cooking.add('preLoader.postcss', {
     test: /\.css$/,
-    loaders: ['postcss']
+    loaders: ['postcss-loader']
   })
 
   cooking.add('postcss', function () {
